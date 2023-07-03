8c3a6e57ff4e69ccc82a61ec0fcd83a2d1d4744c
Ray Chen <rc144@outlook.com>
2023-07-02 22:34:21 -0400

    add stuff

---
+++ content.js
@@ -0,0 +1,104 @@
+document.addEventListener('keyup', function (event) { // keyup may seem less intuitive but it's already the standard in breeze-like scripts and it prevents holding down a key spamming the server
+	if (event.shiftKey || event.ctrlKey || event.altKey || document.activeElement.tagName == 'INPUT' || document.activeElement.tagName == 'TEXTAREA') { // locks you out of the script while you're holding down a modifier key or typing in an input
+		return;
+	} else {
+		switch (event.code) { // event.code is the key that was pressed
+			case 'KeyR': // reload page
+				window.location.reload();
+				break;
+			case 'KeyQ': // go back
+				window.history.back();
+				break;
+			case 'KeyU': // check if you updated
+				window.location.assign("https://www.nationstates.net/page=reports/view=self/filter=change/template-overall=none");
+				break;
+			case 'KeyS': // endorse nation
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByClassName('endorse button icon wa')[0].click();
+				}
+				break;
+			case 'KeyO': // ban nation
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByName('ban')[0].click();
+				}
+				break;
+			case 'KeyK': // eject nation
+				if (window.location.href.includes("nation=")) {
+					document.getElementsByName('eject')[0].click();
+				}
+				break;
+			case 'KeyA': // confirm wa join
+				if (window.location.href.includes("page=join_WA")) {
+					var NationURL = document.getElementsByTagName("form")[1].getElementsByClassName("nlink")[0].href;
+					navigator.clipboard.writeText(NationURL);
+					document.getElementsByClassName('button primary icon approve big')[0].click();				
+				} else {
+					navigator.clipboard.writeText(document.getElementsByClassName('bellink quietlink')[0].href)
+				}
+				break;
+			case 'KeyF': // move to region whose page you're currently on
+				if (window.location.href.includes("region=")) {
+					document.getElementsByName('move_region')[0].click();
+				} else if (window.location.href.includes("change_region")) {
+					document.getElementsByClassName('rlink')[0].click();
+				}
+				break;
+			case 'KeyB': // move to suspicious
+				if (window.location.href == "https://www.nationstates.net/region=suspicious") {
+					document.getElementsByName('move_region')[0].click();
+				} else {
+					window.location.assign("https://www.nationstates.net/region=suspicious");
+				}
+				break;
+			case 'KeyE': // resign from WA
+				if (window.location.href == "https://www.nationstates.net/page=un") {
+					var chk = document.getElementsByName('chk')[0].value;
+					window.location.assign(`https://www.nationstates.net/page=UN_status?action=leave_un&submit=1&chk=${chk}`);
+				} else {
+					window.location.assign("https://www.nationstates.net/page=un");
+				}
+				break;
+			case 'KeyZ': // go to current region page
+				if (window.location.href == "https://www.nationstates.net/page=change_region") { // if on post-relocation page
+					document.getElementsByClassName('info')[0].querySelector('a').click(); // click the region link on the relocation page
+				} else { // otherwise just click the region link through the sidebar
+					document.getElementById('panelregionbar').querySelector('a').click();
+				}
+				break;
+			case 'KeyD': // appoint yourself as and/or deappoint ROs
+				var current_nation = document.getElementById("loggedin").getAttribute("data-nname");
+				// If on the regional control page, open own regional officer page
+				if (window.location.href == "https://www.nationstates.net/page=region_control") {
+					window.location.assign("https://www.nationstates.net/page=regional_officer/nation=" + current_nation);
+				}
+				// If on on own regional officer page, assign officer role
+				else if (window.location.href == "https://www.nationstates.net/page=regional_officer/nation=" + current_nation) {
+					document.getElementsByName("office_name")[0].value = "moxxie cultist";
+					document.getElementsByName("authority_A")[0].checked = true;
+					document.getElementsByName("authority_C")[0].checked = true;
+					document.getElementsByName("authority_E")[0].checked = true;
+					document.getElementsByName("authority_P")[0].checked = true;
+					document.getElementsByName('editofficer')[0].click();
+				}
+				// If on someone else's regional officer page, dismiss them/remove all their powers if successor
+				else if (window.location.href.includes("regional_officer")) {
+					if(document.getElementsByName("authority_S")[0].checked) {
+						document.getElementsByName("authority_A")[0].checked = false;
+						document.getElementsByName("authority_B")[0].checked = false;
+						document.getElementsByName("authority_C")[0].checked = false;
+						document.getElementsByName("authority_E")[0].checked = false;
+						document.getElementsByName("authority_P")[0].checked = false;
+						document.getElementsByName('editofficer')[0].click();
+					} else {
+						document.getElementsByName('abolishofficer')[0].click();
+					}
+					
+				}
+				// If on none of these pages, open regional control page
+				else {
+					window.location.assign("https://www.nationstates.net/page=region_control");
+				}
+				break;
+		} // end switch
+	} // end else
+}); // end event listener
---
+++ readme.txt
@@ -0,0 +1,54 @@
+===============================
+Feather v3.1.0
+
+Derivative extension of SweezeBreeze, with removed and added stuff, tailored towards raiders/tags
+
+================================
+KEY LIST:
+
+A: reload page
+Q: go back
+U: check if you updated
+S: endorse nation
+O: ban nation
+K: eject nation
+R: confirm wa join
+F: move to region whose page you're currently on
+B: move to DBZ
+E: apply/resign to WA
+W: go to current region page
+D: appoint yourself as and/or deappoint ROs
+===============================
+3.1
+prepared for publishing on chrome web store
+updated jump point to suspicious
+changed RO name
+---
+
+
+3.0
+Removed mousetrap, rewritten to use pure javascript instead
+---
+2.2
+Rewrote the script in Mousetrap due to the deprecation of the keyCode method as well as for code cleanliness sake, switched from jQuery in favor of Cash to save ~60KB
+---
+2.1
+Updated to current Feather - Alterations Made By Miravana
+Removed Dossier/Reports Functionality
+Changed Keybinds and Regional Information
+---
+2.0 
+Updated to current SweezeBreeze - Script By Sweeze
+---
+1.2
+Re-arranged keys so less hand moving necessary.
+Added icon.
+---
+1.1
+added regional officer functionality
+---
+1.0
+added confirm WA join feature
+fixed join/resign feature
+removed dossier+defender stuff
+changed spear danes to plum island+le club des cinq
